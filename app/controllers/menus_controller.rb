fc9b16ac37758c501c71b94b7faee21613a0cd9d
中村真 <abcmjqm@gmail.com>
2022-03-14 13:25:17 +0000

    [Add]フォロー・フォロワー機能実装

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -25,7 +25,29 @@ class MenusController < ApplicationController
 
   def show
     @menu = Menu.find(params[:id])
+    #その日の献立を取得
     @menus = Menu.where(start_time: @menu.start_time.in_time_zone.all_day)
+    # 同じ材料の時に加算されるように以下を記述
+    # 空の変数宣言（後で使う用）
+    @foods = {}
+    @menus.each do |menu|
+      menu.recipe.recipe_foods.each do |recipe_food|
+        #recipe_food.food.idに紐づく@foodsのデータがなかったら
+        if @foods[recipe_food.food.id].nil?
+          #空の変数宣言
+          @foods[recipe_food.food.id] = {}
+          # 名称
+          @foods[recipe_food.food.id]['food_name'] = recipe_food.food.food_name
+          #後で数量加算するときにエラーにならないようにとりあえず数量0を突っ込む
+          @foods[recipe_food.food.id]['quantity'] = recipe_food.quantity.to_i
+          # 単位
+          @foods[recipe_food.food.id]['unit'] = recipe_food.food.unit
+        else
+          # 分量を加算
+          @foods[recipe_food.food.id]['quantity'] += recipe_food.quantity.to_i
+        end
+      end
+    end
   end
 
   private
