a1266c2d4281bd927fdea9ad50b2d08e38e4a5ef
中村真 <abcmjqm@gmail.com>
2022-03-19 12:40:58 +0000

    [Add]ランキング機能実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,8 @@
 class HomesController < ApplicationController
-  
+
   def top
+    @favorite_ranks = Recipe.find(Favorite.group(:recipe_id).order('count(recipe_id) desc').limit(10).pluck(:recipe_id))
+    @follower_ranks = User.find(Relationship.group(:followed_id).order('count(followed_id) desc').limit(10).pluck(:followed_id))
   end
 
   def about
