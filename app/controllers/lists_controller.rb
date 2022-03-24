bc4acf12896527b56bc84c2deb441c52e716513f
中村真 <abcmjqm@gmail.com>
2022-03-24 08:35:10 +0000

    [Fix]レイアウト修正

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,14 +9,14 @@ class ListsController < ApplicationController
     @list = List.new(user_id: current_user.id)
     @list.recipe_id = @recipe.id
     @list.save
-    redirect_to request.referer, notice: 'Successful registration of my list!'
+    flash[:notice] = 'Successful registration of my list!'
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
     list = @recipe.lists.find_by(user_id: current_user.id)
     list.destroy
-    redirect_to request.referer, notice: 'Successful removal from my list!'
+    flash[:notice] = 'Successful removal from my list!'
   end
 
 end
