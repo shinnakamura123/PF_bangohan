98812969beef25fdcccac972a7d1cc15f8418853
中村真 <abcmjqm@gmail.com>
2022-03-26 17:36:32 +0000

    [Fix]本番環境でのエラー改善１

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -9,11 +9,3 @@ RSpec.describe User, "モデルに関するテスト", type: :model do
        end
   end
 end
-
-RSpec.describe Recipe, "モデルに関するテスト", type: :model do
-  describe '実際に保存してみる' do
-      it "有効な投稿内容の場合は保存されるか" do
-        expect(FactoryBot.build(:recipe)).to be_valid
-       end
-  end
-end
