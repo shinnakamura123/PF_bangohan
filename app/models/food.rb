bf939dca101cbfac5f172a5ba0070a0b0174dbdc
中村真 <abcmjqm@gmail.com>
2022-03-27 15:06:30 +0000

    [Fix]本番環境でのレイアウト崩れ修正　

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,5 +2,5 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods, dependent: :destroy
 
-  validates :food_name, presence: true
+  validates :food_name, presence: true, uniqueness: true
 end
