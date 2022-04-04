6e331a866546bdd8000b1bd435a4a412a0e32505
中村真 <abcmjqm@gmail.com>
2022-04-04 06:45:10 +0000

    [Fix]文字折り返し

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -31,7 +31,7 @@ class Recipe < ApplicationRecord
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg', size: "400*400")
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
